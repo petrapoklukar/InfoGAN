007ba3eae0fda9da11cdb2291579406ba86d2e52
petrapoklukar <petra.poklukar@gmail.com>
2019-11-29 17:31:09 +0100

    Updated training loop

--- train_robot_traj.py
+++ train_robot_traj.py
@@ -9,8 +9,8 @@ Created on Thu Nov 21 09:18:18 2019
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-import autoencoder_models
-import variational_autoencoder as vae
+import InfoGAN_models
+import InfoGAN as InfoGAN
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,14 +27,12 @@ parser.add_argument('--latent-size', default=5, type=int, help='the dimension of
 parser.add_argument('--num-epoch', default=10000, type=int, help='the number of epochs to train the model')
 parser.add_argument('--snapshot', default=100, type=int, help='the number of epochs to make a snapshot of the training')
 parser.add_argument('--batch-size', default=100, type=int, help='the batch size')
-parser.add_argument('--beta-min', default=0.0, type=float, help='the starting value for beta')
-parser.add_argument('--beta-max', default=0.01, type=float, help='the final value for beta')
-parser.add_argument('--beta-steps', default=20, type=int, help='the number of steps to increase beta')
 parser.add_argument('--device', default=None, type=str, help='the device for training, cpu or cuda')
 
+
 class TrajDataset(Dataset):
-    def __init__(self, data_filename, device="cpu"):
-        self.device = device
+    def __init__(self, data_filename):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.data = torch.from_numpy(np.load(data_filename)).float().to(self.device)
         self.num_samples = self.data.shape[0]
         
@@ -44,41 +42,36 @@ class TrajDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx]
 
+
 if __name__ == '__main__':
     args = parser.parse_args()
 
     latent_size = args.latent_size
 
     if args.device == None:
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     else:
         device = args.device
     print('Device is: {}'.format(device))
 
     traj_dataset = TrajDataset(args.path_to_data, device)
-    trajectory_loader = DataLoader(traj_dataset, batch_size=args.batch_size,shuffle=True, num_workers=0)
+    trajectory_loader = DataLoader(traj_dataset, batch_size=args.batch_size,
+                                   shuffle=True, num_workers=0)
     traj_iter = iter(trajectory_loader)
     x = traj_iter.next().to('cpu').numpy()
 
     n_joints = x.shape[1]
     traj_length = x.shape[2]
 
-
-    # making the autoencoder
-    encoder = autoencoder_models.FullyConnecteEncoder(n_joints*traj_length,latent_size)
-    decoder = autoencoder_models.FullyConnecteDecoder(latent_size,n_joints*traj_length)
-
-    v_autoencoder = vae.VariationalAutoEncoder(encoder, decoder,path_to_model=args.path_to_model,
-                                                device=device,n_epoch=args.num_epoch,
-                                                beta_steps=args.beta_steps, beta_min=args.beta_min, beta_max=args.beta_max,
-                                                snapshot=args.snapshot, lr=0.001)
-    v_autoencoder = v_autoencoder.to(device)
-
-    # train the model
+    # Init the model
+    model = InfoGAN(config)
+    
+    # Train the model
     if args.train:
-        v_autoencoder.train_vae(trajectory_loader)
+        model.train_infogan(trajectory_loader)
 
-    # evaluate the model
+    # Evaluate the model
+    # TODO
     if args.eval:
         if not args.train:
             v_autoencoder.load_model(args.model_filename)
