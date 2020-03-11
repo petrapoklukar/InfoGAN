68a3e2091b9f1fe62cf137186b6f8ee7937fcd21
petrapoklukar <petra.poklukar@gmail.com>
2020-03-11 11:07:27 +0100

    Restructured models

--- train_mnist.py
+++ train_mnist.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import InfoGAN_models
-import InfoGAN_general_A4 as model
+import InfoGAN_general_A5 as model
 
 import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
