Fixed bug in PR calculation

test_linearity.py
@@ -77,7 +77,7 @@ def test_linearity(Z, S, split=True):
         S_pred = reg.predict(Z)
         mse = round(mean_squared_error(S, S), 3)
         reg_score = round(reg.score(Z, S), 3)
-        
+    print(mse, reg_score)
     return mse, reg_score
 
 def compute_rank():    
@@ -177,15 +177,15 @@ if False:
     f2_ax1 = fig2.add_subplot(gs[0, 0])
     f2_ax1.set_yticks(np.arange(len(vae_sgroup1)) + 1)
     f2_ax1.set_yticklabels(vae_sgroup1[::-1])
-#    f2_ax1.set_xlim((0.99, 1.0))
-    f2_ax1.set_xlim(xlim)
+    f2_ax1.set_xlim((0.99, 1.0))
+#    f2_ax1.set_xlim(xlim)
     
     vae_sgroup2 = sorted(vae_group2, key=lambda x: lin_scores[x][key], reverse=True)
     f2_ax2 = fig2.add_subplot(gs[0, 1])
     f2_ax2.set_yticks(np.arange(len(vae_sgroup2)) + 1)
     f2_ax2.set_yticklabels(vae_sgroup2[::-1])
-#    f2_ax2.set_xlim((0.99, 1.0))
-    f2_ax2.set_xlim(xlim)
+    f2_ax2.set_xlim((0.99, 1.0))
+#    f2_ax2.set_xlim(xlim)
  
     gan_sgroup1 = sorted(gan_group1, key=lambda x: lin_scores[x][key], reverse=True)
     f2_ax3 = fig2.add_subplot(gs[1, 0])
@@ -260,11 +260,17 @@ if False:
              ipr_rank_dict['rec'], dis_rank_dict['prec'],
              dis_rank_dict['rec']], axis=0), 3)
     
+    score_sep = np.round(np.sum(
+            [lin_rank_dict['total'], ipr_rank_dict['prec'],
+             ipr_rank_dict['rec'], dis_rank_dict['prec'],
+             dis_rank_dict['rec']], axis=0), 3)
+    
     ratio = 1
     score_sep2 = np.round(np.mean(
             [lin_rank_dict['total'], 
-             ratio * np.array(ipr_rank_dict['rec']) + (1- 2) * np.array(ipr_rank_dict['prec']),
-             0.5 * np.array(dis_rank_dict['prec']) + (1- 0.5) * np.array(dis_rank_dict['rec'])], axis=0), 3)
+             ratio * np.array(ipr_rank_dict['rec']) + (1- 1) * np.array(ipr_rank_dict['prec']),
+#             0.5 * np.array(dis_rank_dict['prec']) + (1- 0.5) * np.array(dis_rank_dict['rec'])
+             dis_rank_dict['total']], axis=0), 3)
     
     score_tot = np.round(np.mean(
             [lin_rank_dict['total'], ipr_rank_dict['total'], 
@@ -285,6 +291,6 @@ if False:
              dis_rank_dict['total_rank' + key]) / 3, 3)
         
     scores = [score_sep_avg, score_tot_avg, score_sep_min, score_tot_min]
-    for score in [score_sep2, score_tot]:
+    for score in [score_tot]:
         print(sorted(list(zip(model_names, score)), key=lambda x:x[1]))
         

test_pr.py
@@ -63,7 +63,7 @@ class TrajDataset(Dataset):
         
     def descale(self, data):
         unit_scaled = (data.cpu().numpy() + 1)/2
-        descaled = (unit_scaled.reshape(-1, 7, 79)) * (self.max - self.min) + self.min
+        descaled = (unit_scaled.reshape(-1, 7, 79) * (self.max - self.min)) + self.min
         return descaled
         
     def __len__(self):
@@ -123,9 +123,6 @@ def get_infogan_samples(config_name, ld, n_prd_samples, chpnt=''):
     # Get the sampled np array
     with torch.no_grad():
         z_noise = torch.empty((n_prd_samples, ld), device=device).uniform_(-1, 1)
-        print(z_noise.shape)
-        print(z_noise.dtype)
-        print(type(z_noise))
         eval_data = model(z_noise).detach()
         eval_np = test_dataset.descale(eval_data).reshape(-1, 7, 79)
     return eval_np
@@ -164,8 +161,8 @@ if __name__ == '__main__':
     yumi_gan_models = {model: index_to_ld(int(model.split('_')[0][-1])) \
         for model in gan_configs}
     yumi_vae_models = {'vae' + str(i): index_to_ld(i) for i in range(1, 10)}
-    evaluate = False
-    analyse = True
+    evaluate = True
+    analyse = False
     
     if analyse:
         n_samples = 2000
@@ -452,10 +449,11 @@ if __name__ == '__main__':
                          
     
     if evaluate:
-        max_ind = 10000
+        max_ind = 15750
         
-        for n_points in [2000, 5000]:
+        for n_points in [2000, 5000, 10000, 15000]:
             print('Chosen n_points: ', n_points)
+            nhood_sizes = [3, 20, 25, 30, 50, 100]
             base = np.random.choice(max_ind, n_points, replace=False)
             ref_np = get_ref_samples(base)
             
@@ -463,9 +461,10 @@ if __name__ == '__main__':
             for model, ld in yumi_gan_models.items():
                 print('InfoGAN model with ld: ', model, ld)
                 infogan_eval_np = get_infogan_samples(model, ld, n_points)
-                
-#                infogan_eval_np_avg15 = moving_average(infogan_eval_np, n=15)
+
+                infogan_eval_np_avg5 = moving_average(infogan_eval_np, n=5)                
                 infogan_eval_np_avg10 = moving_average(infogan_eval_np, n=10)
+                infogan_eval_np_avg15 = moving_average(infogan_eval_np, n=15)
                 
                 print('Starting to calculate InfoGAN PR....')
                 sess = tf.Session()
@@ -473,26 +472,42 @@ if __name__ == '__main__':
                     res0_gan = iprd.knn_precision_recall_features(
                                 ref_np.reshape(-1, 7*79), 
                                 infogan_eval_np.reshape(-1, 7*79), 
-                                nhood_sizes=[20,25,30,50],
+                                nhood_sizes=nhood_sizes,
                                 row_batch_size=500, col_batch_size=100, num_gpus=1)
                     
+                    
+                    res5_gan = iprd.knn_precision_recall_features(
+                                ref_np.reshape(-1, 7*79), 
+                                infogan_eval_np_avg5.reshape(-1, 7*79), 
+                                nhood_sizes=nhood_sizes,
+                                row_batch_size=500, col_batch_size=100, num_gpus=1)
                     
                     res10_gan = iprd.knn_precision_recall_features(
                                 ref_np.reshape(-1, 7*79), 
                                 infogan_eval_np_avg10.reshape(-1, 7*79), 
-                                nhood_sizes=[20,25,30,50],
+                                nhood_sizes=nhood_sizes,
+                                row_batch_size=500, col_batch_size=100, num_gpus=1)
+                    
+                    res15_gan = iprd.knn_precision_recall_features(
+                                ref_np.reshape(-1, 7*79), 
+                                infogan_eval_np_avg15.reshape(-1, 7*79), 
+                                nhood_sizes=nhood_sizes,
                                 row_batch_size=500, col_batch_size=100, num_gpus=1)
                     
                     
                 final_dict[model] = {
                         'res0_gan': res0_gan,
+                        'res5_gan': res5_gan,
                         'res10_gan': res10_gan,
+                        'res15_gan': res15_gan,
+                        'nhoods': nhood_sizes,
+                        'npoints': n_points
                         }
                     
             #_nhood file: res15 & res keys [5, 10, 25, 50]
             for model, ld in yumi_vae_models.items():
                 print('VAE model with ld: ', model, ld)
-                vae_eval_np = get_vae_samples('vae1', 2, n_points)
+                vae_eval_np = get_vae_samples(model, ld, n_points)
     
                 print('Starting to calculate InfoGAN PR....')
                 sess = tf.Session()
@@ -500,11 +515,14 @@ if __name__ == '__main__':
                     res0_vae = iprd.knn_precision_recall_features(
                                 ref_np.reshape(-1, 7*79), 
                                 vae_eval_np.reshape(-1, 7*79), 
-                                nhood_sizes=[15,20,25,30,50],
+                                nhood_sizes=nhood_sizes,
                                 row_batch_size=500, col_batch_size=100, num_gpus=1)
             
-                final_dict[model] = {'res0_vae': res0_vae}
+                final_dict[model] = {
+                    'res0_vae': res0_vae,
+                    'nhoods': nhood_sizes,
+                    'npoints': n_points}
                 
             print('Results ready ', final_dict)
-            with open('test_pr/ipr_results_nhoodFinal_{0}samples.pkl'.format(n_points), 'wb') as f:
+            with open('test_pr/ipr_results_nhood_{0}samples.pkl'.format(n_points), 'wb') as f:
                 pickle.dump(final_dict, f)